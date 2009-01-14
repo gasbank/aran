e30d795c5d95bd4556d49decfaf476eec59366bb
Geoyeob Kim <gasbank@gmail.com>
2009-01-14 11:03:00 +0000

    - HAT biped changed.
    git-svn-id: svn://gasbank.kaist.ac.kr/aran/trunk@187 62270530-ab42-0410-9ef6-e3964f34a1eb

--- Src/EmptyProject/HatBiped.cpp
+++ Src/EmptyProject/HatBiped.cpp
@@ -26,12 +26,11 @@
   \-/   \-/
 
 */
-const unsigned leg_num = 2;
-const unsigned link_num = 3;
+enum BodyEnum { BE_HAT, BE_GROIN_R, BE_GROIN_L, BE_CALF_R, BE_CALF_L, BE_FOOT_R, BE_FOOT_L, BE_COUNT };
+enum JointEnum { JE_R1, JE_L1, JE_R2, JE_L2, JE_R3, JE_L3, JE_COUNT };
 
-MyObject g_leg[leg_num][link_num];
-MyObject g_torso;
-
+MyObject g_objects[BE_COUNT];
+MyObject g_joints[JE_COUNT];
 
 // Mass distribution [kg]
 const dReal torso_m = 40.0;
@@ -39,58 +38,99 @@ const dReal groin_m = 10.0;
 const dReal calf_m = 8.0;
 const dReal foot_m = 2;
 
-// Length [m]
-const dReal torso_h = 0.5;
-const dReal torso_w = 0.5;
-const dReal groin_len = 0.4;
-const dReal calf_len = 0.4;
-const dReal leg_len = groin_len + calf_len;
-const dReal foot_len = 0.275;
-const dReal sagittal_depth = 0.25;
+// Length, radius [m]
+// Torso and foot are box shaped
+const dReal torso_w = 0.4;
+const dReal torso_h = 0.8;
+const dReal torso_d = 0.2;
 
-// Radius [m]
-const dReal groin_r = 0.2;
-const dReal calf_r = 0.17;
-const dReal foot_r = 0.14;
+const dReal foot_w = 0.15;
+const dReal foot_h = 0.1;
+const dReal foot_d = 0.3;
 
-// Link position x-axis
-const dReal x[leg_num][link_num] = {
-	{  torso_w,  torso_w,  torso_w }, // Left leg
-	{ -torso_w, -torso_w, -torso_w } // Right leg
-};
-// Link position y-axis
-const dReal y[leg_num][link_num] = {
-	{ leg_len + 0.1, calf_len + 0.1, 0.1 },
-	{ leg_len + 0.1, calf_len + 0.1, 0.1 }
-};
-// Link position z-axis
-const dReal z[leg_num][link_num] = {
-	{ 0, 0, 0 },
-	{ 0, 0, 0 }
-};
+// Groin and calf are capsule shaped
+const dReal groin_r = 0.15/2;
+const dReal groin_h = 0.5;
 
-// Center of joints x-axis
-const dReal c_x[leg_num][link_num] = {
-	{  torso_w,  torso_w,  torso_w }, // Left leg
-	{ -torso_w, -torso_w, -torso_w } // Right leg
-};
-// Center of joints y-axis
-const dReal c_y[leg_num][link_num] = {
-	{ leg_len + 0.1, calf_len + 0.1, 0.1 },
-	{ leg_len + 0.1, calf_len + 0.1, 0.1 }
-};
-// Center of joints z-axis
-const dReal c_z[leg_num][link_num] = {
-	{ 0, 0, 0 },
-	{ 0, 0, 0 }
-};
+const dReal calf_r = 0.15/2;
+const dReal calf_h = 0.5;
 
-const dReal r[link_num]          =  { groin_r, calf_r, foot_r };
-const dReal length[link_num]     =  { groin_len, calf_len, foot_len };
-const dReal weight[link_num]     =  { groin_m, calf_m, foot_m };
-dReal axis_x[leg_num][link_num] = { { 0, 1, 0 }, { 0, 1, 0 } };
-dReal axis_y[leg_num][link_num] = { { 1, 0, 1 }, { 1, 0, 1 } };
-dReal axis_z[leg_num][link_num] = { { 0, 0, 0 }, { 0, 0, 0 } };
+const dReal leg_h = groin_h + calf_h;
+
+
+double bodyCenters[BE_COUNT][3] = 
+{
+	{ 0, 0, foot_h + leg_h + torso_h/2 }, // HAT
+	{ torso_w/2, 0, foot_h + calf_h + groin_h/2 }, // GROIN_R
+	{ -torso_w/2, 0, foot_h + calf_h + groin_h/2 }, // GROIN_L
+	{ torso_w/2, 0, foot_h + calf_h/2 }, // CALF_R
+	{ -torso_w/2, 0, foot_h + calf_h/2 }, // CALF_L
+	{ torso_w/2, foot_d/2 - calf_r, foot_h/2 }, // FOOT_R
+	{ -torso_w/2, foot_d/2 - calf_r, foot_h/2 } // FOOT_L
+};
+double bodyRadiusAndHeight[BE_COUNT][2] =
+{
+	{ -1, -1 },
+	{ groin_r, groin_h },
+	{ groin_r, groin_h },
+	{ calf_r, calf_h },
+	{ calf_r, calf_h },
+	{ -1, -1 },
+	{ -1, -1 }
+};
+double bodyWeights[BE_COUNT] = { torso_m, groin_m, groin_m, calf_m, calf_m, foot_m, foot_m };
+double jointCenters[JE_COUNT][3] =
+{
+	{ torso_w/2, 0, foot_h + leg_h },
+	{ -torso_w/2, 0, foot_h + leg_h },
+	{ torso_w/2, 0, foot_h + calf_h },
+	{ -torso_w/2, 0, foot_h + calf_h },
+	{ torso_w/2, 0, foot_h },
+	{ -torso_w/2, 0, foot_h }
+};
+unsigned jointLinks[JE_COUNT][2] =
+{
+	{ BE_HAT, BE_GROIN_R },
+	{ BE_HAT, BE_GROIN_L },
+	{ BE_GROIN_R, BE_CALF_R },
+	{ BE_GROIN_L, BE_CALF_L },
+	{ BE_CALF_R, BE_FOOT_R },
+	{ BE_CALF_L, BE_FOOT_L },
+};
+//
+//// Link position y-axis
+//const dReal y[leg_num][link_num] = {
+//	{ leg_len + 0.1, calf_len + 0.1, 0.1 },
+//	{ leg_len + 0.1, calf_len + 0.1, 0.1 }
+//};
+//// Link position z-axis
+//const dReal z[leg_num][link_num] = {
+//	{ 0, 0, 0 },
+//	{ 0, 0, 0 }
+//};
+//
+//// Center of joints x-axis
+//const dReal c_x[leg_num][link_num] = {
+//	{  torso_w,  torso_w,  torso_w }, // Left leg
+//	{ -torso_w, -torso_w, -torso_w } // Right leg
+//};
+//// Center of joints y-axis
+//const dReal c_y[leg_num][link_num] = {
+//	{ leg_len + 0.1, calf_len + 0.1, 0.1 },
+//	{ leg_len + 0.1, calf_len + 0.1, 0.1 }
+//};
+//// Center of joints z-axis
+//const dReal c_z[leg_num][link_num] = {
+//	{ 0, 0, 0 },
+//	{ 0, 0, 0 }
+//};
+//
+//const dReal r[link_num]          =  { groin_r, calf_r, foot_r };
+//const dReal length[link_num]     =  { groin_len, calf_len, foot_len };
+//const dReal weight[link_num]     =  { groin_m, calf_m, foot_m };
+//dReal axis_x[leg_num][link_num] = { { 0, 1, 0 }, { 0, 1, 0 } };
+//dReal axis_y[leg_num][link_num] = { { 1, 0, 1 }, { 1, 0, 1 } };
+//dReal axis_z[leg_num][link_num] = { { 0, 0, 0 }, { 0, 0, 0 } };
 
 
 
@@ -99,103 +139,90 @@ void renderBiped(IDirect3DDevice9* pd3dDevice)
 	D3DXMATRIX worldMat;
 	D3DXMATRIX transMat, rotMat;
 	
-	// Render a torso
-	const dReal* torsoPos = dBodyGetPosition(g_torso.body);
-	const dReal* torsoRot = dBodyGetRotation(g_torso.body);
-	D3DXMatrixTranslation(&transMat, (float)torsoPos[0], (float)torsoPos[1], (float)torsoPos[2]);
-	OdeMatrixToDx(rotMat, torsoRot);
-	worldMat = transMat * rotMat;
-	pd3dDevice->SetTransform(D3DTS_WORLD, &worldMat);
-	g_torso.mesh->DrawSubset(0);
-	
-	unsigned i, j;
-	dReal r,length;
-	for (i = 0; i < leg_num; i++)
+	unsigned i;
+	for (i = 0; i < BE_COUNT; ++i)
 	{
-		for (j = 0; j < link_num; j++ )
-		{
-			dGeomCapsuleGetParams(g_leg[i][j].geom, &r, &length);
+		const dReal* pos = dBodyGetPosition(g_objects[i].body);
+		const dReal* rot = dBodyGetRotation(g_objects[i].body);
 
-			const dReal* pos = dBodyGetPosition(g_leg[i][j].body);
-			const dReal* rot = dBodyGetRotation(g_leg[i][j].body);
-			D3DXMatrixTranslation(&transMat, (float)pos[0], (float)pos[1], (float)pos[2]);
-			OdeMatrixToDx(rotMat, rot);
-			worldMat = rotMat * transMat;
-			pd3dDevice->SetTransform(D3DTS_WORLD, &worldMat);
-			g_leg[i][j].mesh->DrawSubset(0);
-		}
+		D3DXMatrixTranslation(&transMat, (float)pos[0], (float)pos[1], (float)pos[2]);
+		OdeMatrixToDx(rotMat, rot);
+		worldMat = transMat * rotMat;
+		pd3dDevice->SetTransform(D3DTS_WORLD, &worldMat);
+		g_objects[i].mesh->DrawSubset(0);
 	}
+	
 }
 
 void makeBipedMesh(IDirect3DDevice9* pd3dDevice)
 {
 	HRESULT hr;
-	V(D3DXCreateBox(pd3dDevice, (float)torso_w, (float)torso_h, (float)sagittal_depth, &g_torso.mesh, 0));
+	V(D3DXCreateBox(pd3dDevice, (float)torso_w, (float)torso_d, (float)torso_h, &g_objects[BE_HAT].mesh, 0));
 
-	V(D3DXCreateCylinder(pd3dDevice, (float)groin_r, (float)groin_r, (float)groin_len, 16, 2, &g_leg[0][0].mesh, 0));
-	V(D3DXCreateCylinder(pd3dDevice, (float)groin_r, (float)groin_r, (float)groin_len, 16, 2, &g_leg[1][0].mesh, 0));
+	V(D3DXCreateCylinder(pd3dDevice, (float)groin_r, (float)groin_r, (float)groin_h, 16, 2, &g_objects[BE_GROIN_R].mesh, 0));
+	V(D3DXCreateCylinder(pd3dDevice, (float)groin_r, (float)groin_r, (float)groin_h, 16, 2, &g_objects[BE_GROIN_L].mesh, 0));
 
-	V(D3DXCreateCylinder(pd3dDevice, (float)calf_r, (float)calf_r, (float)calf_len, 16, 2, &g_leg[0][1].mesh, 0));
-	V(D3DXCreateCylinder(pd3dDevice, (float)calf_r, (float)calf_r, (float)calf_len, 16, 2, &g_leg[1][1].mesh, 0));
+	V(D3DXCreateCylinder(pd3dDevice, (float)calf_r, (float)calf_r, (float)calf_h, 16, 2, &g_objects[BE_CALF_R].mesh, 0));
+	V(D3DXCreateCylinder(pd3dDevice, (float)calf_r, (float)calf_r, (float)calf_h, 16, 2, &g_objects[BE_CALF_L].mesh, 0));
 
-	V(D3DXCreateCylinder(pd3dDevice, (float)foot_r, (float)foot_r, (float)foot_len, 16, 2, &g_leg[0][2].mesh, 0));
-	V(D3DXCreateCylinder(pd3dDevice, (float)foot_r, (float)foot_r, (float)foot_len, 16, 2, &g_leg[1][2].mesh, 0));
+	V(D3DXCreateBox(pd3dDevice, (float)foot_w, (float)foot_d, (float)foot_h, &g_objects[BE_FOOT_R].mesh, 0));
+	V(D3DXCreateBox(pd3dDevice, (float)foot_w, (float)foot_d, (float)foot_h, &g_objects[BE_FOOT_L].mesh, 0));
 }
 void releaseBipedMesh()
 {
-	SAFE_RELEASE(g_torso.mesh);
-
-	unsigned i, j;
-	for (i = 0; i < leg_num; ++i)
-		for (j = 0; j < link_num; ++j)
-			SAFE_RELEASE(g_leg[i][j].mesh);
+	unsigned i;
+	for (i = 0; i < BE_COUNT; ++i)
+	{
+		SAFE_RELEASE(g_objects[i].mesh);
+	}
 }
 
 
 void makeBipedBody(dWorldID world, dSpaceID space)
 {
-	// Crate a torso
+	// Crate a HAT(torso-arm-trunk)
 	dMass mass;
-	g_torso.body  = dBodyCreate(world);
+	g_objects[BE_HAT].body = dBodyCreate(world);
 	dMassSetZero(&mass);
-	dMassSetBoxTotal(&mass, torso_m, torso_w, torso_h, sagittal_depth);
-	dBodySetMass(g_torso.body, &mass);
-	g_torso.geom = dCreateBox(space,torso_w, torso_h, sagittal_depth);
-	dGeomSetBody(g_torso.geom, g_torso.body);
-	dBodySetPosition(g_torso.body, 0, 0, leg_len + 0.1 + torso_h/2);
+	dMassSetBoxTotal(&mass, torso_m, torso_w, torso_d, torso_h);
+	dBodySetMass(g_objects[BE_HAT].body, &mass);
+	g_objects[BE_HAT].geom = dCreateBox(space, torso_w, torso_d, torso_h);
+	dGeomSetBody(g_objects[BE_HAT].geom, g_objects[BE_HAT].body);
+	dBodySetPosition(g_objects[BE_HAT].body, bodyCenters[BE_HAT][0], bodyCenters[BE_HAT][1], bodyCenters[BE_HAT][2]);
 
-	// Create legs
+	// Create groins, calfs (HAT and feet are excluded)
+	unsigned i;
+	for (i = 1; i < BE_COUNT - 2; i++)
+	{
+		g_objects[i].body = dBodyCreate(world);
+		dBodySetPosition(g_objects[i].body, bodyCenters[i][0], bodyCenters[i][1], bodyCenters[i][0]);
+		dMassSetZero(&mass);
+		dMassSetCapsuleTotal(&mass, bodyWeights[i], 3 /* Length in z-axis */, bodyRadiusAndHeight[i][0], bodyRadiusAndHeight[i][1]);
+		dBodySetMass(g_objects[i].body, &mass);
+		g_objects[i].geom = dCreateCapsule(space, bodyRadiusAndHeight[i][0], bodyRadiusAndHeight[i][1]);
+		dGeomSetBody(g_objects[i].geom, g_objects[i].body);
+	}
+	// Create feet
 	dMatrix3 R;
 	dRFromAxisAndAngle(R, 1, 0, 0, M_PI/2);  // Rotate 90 deg about x-axis
-	unsigned i, j;
-	for (i = 0; i < leg_num; i++)
+	for (i = BE_COUNT - 2; i < BE_COUNT; i++)
 	{
-		for (j = 0; j < link_num; j++)
-		{
-			g_leg[i][j].body = dBodyCreate(world);
-			if (j == link_num - 1) dBodySetRotation(g_leg[i][j].body, R); // Feet are rotated
-			dBodySetPosition(g_leg[i][j].body, x[i][j], y[i][j], z[i][j]);
-			dMassSetZero(&mass);
-			dMassSetCapsuleTotal(&mass, weight[j], 3, r[j], length[j]);
-			dBodySetMass(g_leg[i][j].body, &mass);
-			g_leg[i][j].geom = dCreateCapsule(space, r[j], length[j]);
-			dGeomSetBody(g_leg[i][j].geom, g_leg[i][j].body);
-		}
+		g_objects[i].body = dBodyCreate(world);
+		dBodySetPosition(g_objects[i].body, bodyCenters[i][0], bodyCenters[i][1], bodyCenters[i][0]);
+		dBodySetRotation(g_objects[i].body, R);
+		dMassSetZero(&mass);
+		dMassSetBoxTotal(&mass, bodyWeights[i], foot_w, foot_d, foot_h);
+		dBodySetMass(g_objects[i].body, &mass);
+		g_objects[i].geom = dCreateBox(space, foot_w, foot_d, foot_h);
+		dGeomSetBody(g_objects[i].geom, g_objects[i].body);
 	}
-
-	// Create links and attach legs to the torso
-	for (i = 0; i < leg_num; i++)
+	
+	// Create joints and link
+	for (i = 0; i < JE_COUNT; i++)
 	{
-		for (j = 0; j < link_num; j++)
-		{
-			g_leg[i][j].joint = dJointCreateHinge(world, 0);
-			if (j == 0)
-				dJointAttach(g_leg[i][j].joint, g_torso.body, g_leg[i][j].body);
-			else
-				dJointAttach(g_leg[i][j].joint, g_leg[i][j-1].body, g_leg[i][j].body);
-
-			dJointSetHingeAnchor(g_leg[i][j].joint, c_x[i][j], c_y[i][j], c_z[i][j]);
-			dJointSetHingeAxis(g_leg[i][j].joint, axis_x[i][j], axis_y[i][j],axis_z[i][j]);
-		}
+		g_joints[i].joint = dJointCreateHinge(world, 0);
+		dJointAttach(g_joints[i].joint, g_objects[ jointLinks[i][0] ].body, g_objects[ jointLinks[i][1] ].body);
+		dJointSetHingeAnchor(g_joints[i].joint, jointCenters[i][0], jointCenters[i][1], jointCenters[i][2]);
+		dJointSetHingeAxis(g_joints[i].joint, 1, 0, 0); // All hinges rotate by x-axis
 	}
 }
