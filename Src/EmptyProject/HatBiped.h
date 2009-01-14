8a70fcdd133d3f107a0efb8433df9337de5c8c90
Geoyeob Kim <gasbank@gmail.com>
2009-01-14 12:23:45 +0000

    - HatBiped setting completed.
    git-svn-id: svn://gasbank.kaist.ac.kr/aran/trunk@191 62270530-ab42-0410-9ef6-e3964f34a1eb

--- Src/EmptyProject/HatBiped.h
+++ Src/EmptyProject/HatBiped.h
@@ -3,4 +3,5 @@
 void makeBipedBody(dWorldID world, dSpaceID space);
 void renderBiped(IDirect3DDevice9* pd3dDevice);
 void makeBipedMesh(IDirect3DDevice9* pd3dDevice);
+void PcontrolHatBiped();
 void releaseBipedMesh();
