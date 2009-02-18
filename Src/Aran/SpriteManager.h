6cbd798ca99e375369138b9333c1bd1c508cf4a3
Geoyeob Kim <gasbank@gmail.com>
2009-02-18 13:34:50 +0000

    - HatBiped parameters modified.
    git-svn-id: svn://gasbank.kaist.ac.kr/aran/trunk@209 62270530-ab42-0410-9ef6-e3964f34a1eb

--- Src/Aran/SpriteManager.h
+++ Src/Aran/SpriteManager.h
@@ -15,7 +15,16 @@ public:
 	SpriteManager();
 	~SpriteManager(void);
 	
+	/**
+	@brief 등록된 모든 Sprite 의 DrawRequest 를 렌더링한다.
+	@remarks DrawRequest 중 bRender 가 true인 것만 렌더링 된다.
+	*/
 	void frameRender();
+
+	/**
+	@brief 특정한 Sprite 의 DrawRequest 를 렌더링한다.
+	@remarks 여기서 정한 spriteName 의 custom rendering 플레그가 true 여야만 한다.
+	*/
 	void frameRenderSpecificSprite( const char* spriteName );
 	Sprite* registerSprite( const char* spriteName, const char* spriteFileName );
 
