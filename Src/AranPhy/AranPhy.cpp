f9e1ef88431a5f38257da2d75845819bb18e610a
Geoyeob Kim <gasbank@gmail.com>
2009-07-30 10:24:53 +0000

    - AranPhy (Aran library for physics) initiated.
    git-svn-id: svn://gasbank.kaist.ac.kr/aran/trunk@354 62270530-ab42-0410-9ef6-e3964f34a1eb

---
+++ Src/AranPhy/AranPhy.cpp
@@ -0,0 +1,9 @@
+// 2009 Geoyeob Kim
+// AranPhy.cpp : Defines the exported functions for the DLL application.
+//
+// OpenDE (Open Dynamics Engine) Wrapper for Aran library.
+//
+
+#include "AranPhyPCH.h"
+
+
