09d7da63b0c1106f5b571f68645b960f39a1b4a4
Geoyeob Kim <gasbank@gmail.com>
2010-04-10 10:36:27 +0000

    No meaning
    git-svn-id: svn://gasbank.kaist.ac.kr/aran/trunk@541 62270530-ab42-0410-9ef6-e3964f34a1eb

--- pymuscle/lemke.py
+++ pymuscle/lemke.py
@@ -12,7 +12,7 @@ from numpy import *
 def lemke(M, q, z0):
 	n = len(q)
 	assert M.shape == (n,n)
-	z0 = zeros((n)) # z0 is zero-vector for now...z
+	z0 = zeros((n)) # z0 is zero-vector for now...
 	zer_tol = 1e-5
 	piv_tol = 1e-8
 	maxiter = min(1000,50*n)
