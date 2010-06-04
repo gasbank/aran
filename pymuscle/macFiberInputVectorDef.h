4d5ef5311d2ae2a6a4e51acf84bdc6bc51885c09
Geoyeob Kim <gasbank@gmail.com>
2010-06-04 13:53:05 +0000

    C porting WIP
    git-svn-id: svn://gasbank.kaist.ac.kr/aran/trunk@591 62270530-ab42-0410-9ef6-e3964f34a1eb

---
+++ pymuscle/macFiberInputVectorDef.h
@@ -0,0 +1,58 @@
+#ifndef MACFIBERINPUTVECTORDEF_H_INCLUDED
+#define MACFIBERINPUTVECTORDEF_H_INCLUDED
+
+
+/* BODY_ORG RELATED VARIABLES (18) */
+#define px_org input[0]
+#define py_org input[1]
+#define pz_org input[2]
+#define qw_org input[3]
+#define qx_org input[4]
+#define qy_org input[5]
+#define qz_org input[6]
+#define pdx_org input[7]
+#define pdy_org input[8]
+#define pdz_org input[9]
+#define qdw_org input[10]
+#define qdx_org input[11]
+#define qdy_org input[12]
+#define qdz_org input[13]
+#define m_org input[14]
+#define Ixx_org input[15]
+#define Iyy_org input[16]
+#define Izz_org input[17]
+/* BODY_INS RELATED VARIABLES (18) */
+#define px_ins input[18]
+#define py_ins input[19]
+#define pz_ins input[20]
+#define qw_ins input[21]
+#define qx_ins input[22]
+#define qy_ins input[23]
+#define qz_ins input[24]
+#define pdx_ins input[25]
+#define pdy_ins input[26]
+#define pdz_ins input[27]
+#define qdw_ins input[28]
+#define qdx_ins input[29]
+#define qdy_ins input[30]
+#define qdz_ins input[31]
+#define m_ins input[32]
+#define Ixx_ins input[33]
+#define Iyy_ins input[34]
+#define Izz_ins input[35]
+/* FIBER RELATED VARIABLES (12) */
+#define KSE input[36]
+#define KPE input[37]
+#define b input[38]
+#define xrest input[39]
+#define T input[40]
+#define A input[41]
+#define fibbx_org input[42]
+#define fibby_org input[43]
+#define fibbz_org input[44]
+#define fibbx_ins input[45]
+#define fibby_ins input[46]
+#define fibbz_ins input[47]
+
+
+#endif // MACFIBERINPUTVECTORDEF_H_INCLUDED
